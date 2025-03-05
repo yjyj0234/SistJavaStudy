c3c02e98b9150046f9e8d84e24351f7d5558fd2b
yjyj <yjyj0345@naver.com>
2025-03-05 17:55:23 +0900

    0305

---
+++ javaproject/SistJavaProject/src/day0305/Ex1Extends.java
@@ -0,0 +1,88 @@
+package day0305;
+
+//super
+class Car
+{
+	private String carName;
+	private String carColor;
+	//디폴트 생성자(만드려면 부모자녀 둘다 있어야함) 
+	public Car() {
+		
+	}
+	//명시적 생성자
+	public Car(String name,String color)
+	{
+		carName=name;
+		carColor=color;
+	}
+	
+	
+	//setters & getters
+	public String getCarName() {
+		return carName;
+	}
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+	public String getCarColor() {
+		return carColor;
+	}
+	public void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+	//출력메서드
+	public void writeCar()
+	{
+		System.out.println("내 차는 "+carName+"이고 색상은 "+carColor+"입니다");
+	}
+}
+
+//sub
+class MyCar extends Car
+{
+	private int carPrice;
+	
+	//디폴트 생성자
+	public MyCar() {
+		//보이지는 않지만 super() 자동으로 있음
+	}
+	public MyCar(String carName,String carColor,int carPrice) {
+		super(carName, carColor);  //반드시 첫줄
+		this.carPrice=carPrice;
+	}
+	public int getCarPrice() {
+		return carPrice;
+	}
+
+	public void setCarPrice(int carPrice) {
+		this.carPrice = carPrice;
+	}
+	
+	//오버라이딩
+	@Override
+	public void writeCar() {
+		// TODO Auto-generated method stub
+		super.writeCar();
+		System.out.println("내차의 가격은 "+carPrice+"원 입니다");
+	}
+}
+
+public class Ex1Extends {
+
+	public static void main(String[] args) {
+		//자식클래스 디폴트 생성
+		MyCar my1=new MyCar();
+		my1.writeCar();
+				
+		//자식클래스 명시적 생성
+		MyCar my2=new MyCar("스타리아","블랙",30000000);
+		my2.writeCar();
+		System.out.println("my1의 Car값 수정해보기");
+		my1.setCarName("아우디");
+		my1.setCarColor("화이트");
+		my1.setCarPrice(50000000);
+		my1.writeCar();
+		
+	}
+
+}
