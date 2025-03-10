6887d527c5e1cd20752363cd1d0f776137e80008
yjyj <yjyj0345@naver.com>
2025-03-10 18:15:39 +0900

    0310

---
+++ javaproject/SistJavaProject/src/day0310/ThreadEx1.java
@@ -0,0 +1,32 @@
+package day0310;
+
+//스레드가 아닌 상태에서의 실행결과
+public class ThreadEx1 {
+
+	String name;
+	int num;
+	
+	public ThreadEx1(String name,int num) {
+		this.name=name;
+		this.num=num;
+	}
+	
+	public void run() {
+		
+		for(int i=1;i<=num;i++) {
+			System.out.println(name+"==>"+i);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ThreadEx1 th1=new ThreadEx1("하나", 300);
+		ThreadEx1 th2=new ThreadEx1("둘", 300);
+		ThreadEx1 th3=new ThreadEx1("셋", 300);
+		
+		th1.run();
+		th2.run();
+		th3.run();
+	}
+
+}
