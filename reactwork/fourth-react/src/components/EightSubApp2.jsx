6af57892e3ba92c52466dfda4d744a318bc4b0e8
yjyj0234 <yjyj0345@naver.com>
2025-07-23 23:32:33 +0900

    0723

--- reactwork/fourth-react/src/components/EightSubApp2.jsx
+++ reactwork/fourth-react/src/components/EightSubApp2.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EightSubApp2 = (props) => {
-    const p=props;
+  const p=props;
+  const [scolor,setScolor]=useState('#ffffff');
+
+  const handleColorChange=(e)=>{
+    setScolor(e.target.value);
+  }
+  const saveColor=()=>{
+    p.onClick(scolor);
+  }
    
 
   return (
     <div>
       <h3>EightSubApp2_2번째 자식 컴포넌트</h3>
       <b>색상선택:</b>
-      <input type="color" style={{width:'100px'}} />
+      <input type="color" style={{width:'100px'}}  value={scolor} onChange={handleColorChange}/>
 
-      <button type='button' className='btn btn-success' onClick={p.onClick}>색상 추가</button>
+      <button type='button' className='btn btn-success' onClick={saveColor}>색상 추가</button>
     </div>
   )
 }
